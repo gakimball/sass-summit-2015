9fcd7bcd2457426aa75481915e48279c0feae446
Geoff Kimball <geoff@zurb.com>
2015-11-19 01:57:45 -0800

    Show final state of tutorial

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+var $cart = $('#shopping-cart');
+
+$('#shopping-cart-open').click(function() {
+  Foundation.Motion.animateIn($cart, 'hinge-in', function() {
+    $('.shopping-cart-items').addClass('is-animating');
+  });
+});
+
+$('#shopping-cart-close').click(function() {
+  Foundation.Motion.animateOut($cart, 'hinge-out');
+});
